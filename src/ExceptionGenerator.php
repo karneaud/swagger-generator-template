3b82871e27668be291ddfc0c8b8e57c7c7387c9a
Kendall Arneaud <kendall.arneaud@gmail.com>
2023-06-28 17:05:31 +0000

    Added exception generator

---
+++ src/ExceptionGenerator.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Karneaud\Generator;
+
+use SwaggerGen\Generator\AbstractGenerator;
+use SwaggerGen\Generator\GeneratorInterface;
+
+class ResponseGenerator extends AbstractGenerator implements GeneratorInterface
+{
+
+    private $templ = <<<PHP
+    <?php 
+    namespace %s\Exception;
+    class %s extends \Exception {}
+    PHP;
+
+    public function build(array $options)
+    {   
+            $this->addClass('ValidationException', sprintf($this->templ, $this->namespace, 'ValidationException') );
+    }
+
+    public function getNamespace() :string {
+        return "{$this->namespace}\\Exception";
+    }
+}
