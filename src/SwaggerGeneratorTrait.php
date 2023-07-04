ce556f58bc60963c5bdab3b76f33446acc65392c
Kendall Arneaud <kendall.arneaud@gmail.com>
2023-07-04 15:26:16 +0000

    add correct php type

--- src/SwaggerGeneratorTrait.php
+++ src/SwaggerGeneratorTrait.php
@@ -14,13 +14,29 @@ trait SwaggerGeneratorTrait {
         },'');
     }
 
+    function fixType($type = 'string') {
+        switch(true) {
+            case in_array($type,['integer','number']): 
+                $type = 'int'; break;
+            case $type == 'boolean': 
+                $type = 'bool'; break;
+            case $type == 'double':
+                $type = 'float'; break;
+            case !in_array($type,['int','bool','array']):
+                $type = 'string';
+            default : break;
+        }
+
+        return $type;
+    }
+
     private function extractParameters(array $openapi, string $path, string $method = 'get')
     {
         $parameters = [];
         $details = $openapi['paths'][$path][$method];
         $pathParameters = $details['parameters'] ?? [];
         foreach ($pathParameters as $pathParameter) {
-            $parameters[$pathParameter['name']] = $pathParameter['schema']['type'];
+            $parameters[$pathParameter['name']] = $this->fixType($pathParameter['schema']['type']);
         }
 
         if(!empty($requestBody = ($details['requestBody'] ?? []))){
@@ -29,10 +45,11 @@ trait SwaggerGeneratorTrait {
                 $ref = $requestBody['content']['application/json']['schema']['$ref'];
                 $componentSchema = $openapi['components']['schemas'][ substr($ref,strrpos($ref,'/') +1 )];
                 $parameters = array_merge($parameters, array_map(fn($prop) => $prop['type'] ?? null,$componentSchema['properties']));
+                $parameters = array_map([$this,'fixType'],array_filter($parameters));
             } else if(isset($requestBody['content']['application/json']['schema']['properties'])) {
                 $requestBodyParameters = $requestBody['content']['application/json']['schema']['properties'];
                 foreach ($requestBodyParameters as $key => $requestBodyParameter) {
-                    $parameters[$key] = $requestBodyParameter['type'];
+                    $parameters[$key] = $ths->fixType($requestBodyParameter['type']);
                 }
             }
         }
